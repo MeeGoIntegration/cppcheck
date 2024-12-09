82dc8971f2a23afac5aa7aae848e4bce2ceeb280
Martin Kampas <martin.kampas@seafarix.com>
2024-12-09 20:33:22 +0100

    docker: Let clang-tidy find system includes from SDK
    This should be done with the help of the options like
    '--gcc-installation-dir' and '--gcc-triple', but I couldn't make it
    work.

--- clang-tidy.sh
+++ clang-tidy.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
+# Hack: Add some include paths speficically for clang-tidy. These are not
+# needed for successful cppcheck execution and at the same time adding them on
+# cppcheck command line would multiply the number of configurations to check.
 
-clang-tidy-16 "$@" |tee /dev/stderr
+set -o nounset
+
+SDKROOT=${SDKTARGETSYSROOT%/sysroots/*}
+
+SYSINCL=$(
+    set +o nounset
+    . "$SDKROOT/environment-setup-aarch64-gnu-linux"
+    set -o nounset
+    aarch64-gnu-linux-g++ --sysroot "$SDKTARGETSYSROOT" -x c++ -E -Wp,-v - </dev/null \
+        |& sed -n '/^#include <...> search/,/^End of search/ { /^ \//p }' \
+        |sed 's/^ /-I/'
+)
+
+MAYBE_SEP=
+[[ " $* " == *" -- "* ]] || MAYBE_SEP=--
+
+clang-tidy-16 "$@" $MAYBE_SEP $SYSINCL |tee /dev/stderr
 
 # Let cppcheck use also partiall results. It would discard its output if it
 # exited with non-zero.
